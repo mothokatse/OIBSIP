2ebf2849bb6bd0916f8d44374c7b3b1d3fc0f219
mothokatse <157685592+mothokatse@users.noreply.github.com>
2024-03-05 22:49:19 +0200

    Add files via upload
    This files are for a landing page for a company called Farm-preneur which is a consultancy agency for pastoral farmers.

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("getStartedBtn").addEventListener("click", function() {
+    // Handle the click event here, for example, scroll to a specific section
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+});
